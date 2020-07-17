8bf830efd1643afb1a7d89a53850e6dfa2acc975
Alex Gomes <alex.gomes.net3@gmail.com>
2020-07-17 09:27:20 -0300

    Install axios and config api instance base

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,61 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard:React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github</Title>
+const Dashboard:React.FC = () => {
+  const [repositories, setRepositories] = useState([]);
 
-    <Form>
-      <input placeholder="Digite aqui" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
 
-    <Repositories>
-      <a href="teste">
-        <img 
-          src="https://avatars2.githubusercontent.com/u/28963634?s=460&u=ae2eb005bde6b39bf0147b86068e69f8d4c62d67&v=4" 
-          alt="Alex Gomes"
-        />
-        <div>
-          <strong>alexgomesnet3/gonode</strong>
-          <p>RocketSeat study project of the Adonis JS Framework</p>
-        </div>
+      <Form>
+        <input placeholder="Digite aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/28963634?s=460&u=ae2eb005bde6b39bf0147b86068e69f8d4c62d67&v=4"
+            alt="Alex Gomes"
+          />
+          <div>
+            <strong>alexgomesnet3/gonode</strong>
+            <p>RocketSeat study project of the Adonis JS Framework</p>
+          </div>
 
-      <a href="teste">
-        <img
-          src="https://avatars2.githubusercontent.com/u/28963634?s=460&u=ae2eb005bde6b39bf0147b86068e69f8d4c62d67&v=4"
-          alt="Alex Gomes"
-        />
-        <div>
-          <strong>alexgomesnet3/gonode</strong>
-          <p>RocketSeat study project of the Adonis JS Framework</p>
-        </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 
-        <FiChevronRight size={20} />
-      </a>
-
-      <a href="teste">
-        <img
-          src="https://avatars2.githubusercontent.com/u/28963634?s=460&u=ae2eb005bde6b39bf0147b86068e69f8d4c62d67&v=4"
-          alt="Alex Gomes"
-        />
-        <div>
-          <strong>alexgomesnet3/gonode</strong>
-          <p>RocketSeat study project of the Adonis JS Framework</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-); 
+}; 
 
 
 export default Dashboard;
