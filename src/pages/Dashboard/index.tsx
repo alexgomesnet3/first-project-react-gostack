d457643520bc4e03e67298c502ee6815f590d8df
Alex Gomes <alex.gomes.net3@gmail.com>
2020-08-09 17:40:29 -0300

    Building routes between pages with React Router Dom

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -72,7 +72,7 @@ const Dashboard:React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <Link key={repository.full_name} to="/repository">
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
